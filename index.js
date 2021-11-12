0aa7c6f7529c245dc4d031f4403db7b88b672576
Muhammad Fahad <fahadsahib786@gmail.com>
2021-11-12 23:05:38 +0500

    btc

--- index.js
+++ index.js
@@ -11,8 +11,21 @@ app.get("/",function (req,res) {
 
 
 app.post("/",function (req,res) {
-   request("https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD", function (error,response,body) {
-       console.log(response.statusCode);
+
+    var Crypto=req.body.Crypto;
+    var Fiat=req.body.Fiat;
+
+    var baseUrl="https://apiv2.bitcoinaverage.com/indices/global/ticker/";
+    var finalUrl=baseUrl+Crypto+Fiat;
+
+
+   request(finalUrl, function (error,response,body) {
+       var data=JSON.parse(body);
+       var price=data.last;
+
+       res.send("<h1>The Current price of "+Crypto+" is "+price+Fiat+"</h1>");
+
+
    });    
 });
 
